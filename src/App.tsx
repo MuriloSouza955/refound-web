7a2ce690a93da09ea065ecbf28b208fcc5e8e9b7
MuriloSouza955 <souza.murilo@gmail.com>
2025-07-29 21:54:13 -0300

    Rotas de autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Routes } from "./routes";
+
 export function App() {
-  return <h1 className="text-2xl font-bold text-gray-200">Refound Web</h1>;
+  return <Routes />;
 }
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Input } from "../components/input";
+import { Button } from "../components/Button";
+
+export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    console.log(name, email, password, confirmPassword);
+
+    alert("Enviado");
+  }
+  return (
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="Name"
+        placeholder="Seu nome"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Input
+        required
+        legend="Confirmação da senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Cadastrar
+      </Button>
+      <a
+        href="/"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-shadow-green-800 transition ease-linear" 
+      >
+        Já tenho uma conta
+      </a>
+    </form>
+  );
+}
